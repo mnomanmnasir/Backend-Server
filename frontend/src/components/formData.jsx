422c48684d17472e30f442f5a854e344c7857190
Noman Muhammad Nasir <102538287+mnomanmnasir@users.noreply.github.com>
2024-04-03 15:09:10 -0700

    working on delete  and update operation

--- frontend/src/components/formData.jsx
+++ frontend/src/components/formData.jsx
@@ -1,6 +1,26 @@
 import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Link, NavLink } from 'react-router-dom'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
+
+  const wordsList = ['Welcome To CRUD Application'];
+
+  const [showModal, setShowModal] = useState(false);
+  const [editingItemId, setEditingItemId] = useState(null);
+  // const formattedWordsList = wordsList.map(word => `- ${word}`);
+
+  const [typingEffect] = useTypewriter({
+    words: wordsList,
+    loop: {},
+    typeSpeed: 100,
+    deleteSpeed: 40,
+  });
   // Define state to store the form data
   const [formData, setFormData] = useState({
     name: "",
@@ -18,11 +38,32 @@ function App() {
       .then((response) => response.json())
       .then((data) => setData(data))
       .catch((error) => console.error("Error fetching data:", error));
+      fetchData()
   }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/api/v1/forms");
+      if (response.ok) {
+        const data = await response.json();
+        setData(data);
+      } else {
+        throw new Error('Failed to fetch data');
+      }
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      toast.error('Failed to fetch data');
+    }
+  };
 
   // Function to handle form submission
   const handleSubmit = async (e) => {
+
     e.preventDefault();
+    if (formData.name === "" || formData.email === "" || formData.message === "") {
+      toast.error('Please fill in all fields');
+      return;
+    }
     try {
       // Make a POST request to store form data
       const response = await fetch("http://localhost:8080/api/v1/forms", {
@@ -34,6 +75,7 @@ function App() {
       });
       if (response.ok) {
         console.log("Form data submitted successfully");
+        toast.success('Form data submitted successfully');
         window.location.reload();
 
         // You can add logic here to show a success message or redirect the user
@@ -54,28 +96,6 @@ function App() {
     });
   };
 
-
-  // Function to handle delete operation
-  // const handleDelete = async (id) => {
-  //   try {
-  //     // Make a DELETE request to delete the item
-  //     const response = await fetch(`http://localhost:8080/api/v1/forms/${id}`, {
-  //       method: "DELETE"
-  //     });
-  //     if (response.ok) {
-  //       console.log("Item deleted successfully");
-  //       // Refresh the data after deletion
-  //       const newData = data.filter(item => item._id.$oid !== id);
-  //       setData(newData);
-  //     } else {
-  //       console.error("Failed to delete item");
-  //     }
-  //   } catch (error) {
-  //     console.error("Error deleting item:", error);
-  //   }
-  // };
-
-
   const handleDelete = async (id) => {
     try {
       let answer = window.confirm(
@@ -95,6 +115,8 @@ function App() {
         // Refresh the data after deletion
         const newData = data.filter((item) => item._id !== id);
         setData(newData);
+        toast.success('Inventory item deleted successfully');
+
         window.location.reload();
       } else {
         console.error("Failed to delete item");
@@ -105,13 +127,68 @@ function App() {
   };
 
 
+
+  const handleEdit = (item) => {
+    setEditingItemId(item._id);
+    setFormData({
+      name: item.name,
+      email: item.email,
+      message: item.message
+    });
+    setShowModal(true);
+  };
+
+  const handleModalClose = () => {
+    setShowModal(false);
+    setEditingItemId(null);
+    setFormData({
+      name: "",
+      email: "",
+      message: ""
+    });
+  };
+
+  const handleModalSubmit = async () => {
+    try {
+      const response = await fetch(`http://localhost:8080/api/v1/forms/${editingItemId}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(formData)
+      });
+      if (response.ok) {
+        console.log("Form data updated successfully");
+        toast.success('Form data updated successfully');
+        setShowModal(false);
+        fetchData();
+      } else {
+        console.error("Failed to update form data");
+        toast.error('Failed to update form data');
+      }
+    } catch (error) {
+      console.error("Error updating form data:", error);
+      toast.error('Error updating form data');
+    }
+  };
+
   return (
     <div className="App">
+      <ToastContainer autoClose={2000} /> {/* Render the ToastContainer component */}
       {/* Conditionally render the table when data is available */}
-      <div className="container overflow-hidden" style={{ marginTop: "5%"}}>
+      <div className="container overflow-hidden" style={{ marginTop: "5%" }}>
         <div className="row">
           <div className="col">
-            <h1 style={{ textAlign: "left" }}>CRUD APPLICATION</h1>
+            <div className="typewriter-container text-center position-absolute top-0 start-50 translate-middle" style={{ zIndex: 1, fontFamily: 'Protest Guerrilla, sans-serif', fontSize: '34px', marginTop: '5%', marginLeft: '-10px', fontWeight: 'bold', color: 'green', fontStyle: 'italic' }}>
+
+              <ul style={{ listStyleType: 'none', padding: 0 }}>
+                {typingEffect.split('\n').map((word, index) => (
+                  <li key={index}>
+                    {index === 0 ? '' : ''} {word} <Cursor />
+                  </li>
+                ))}
+              </ul>
+            </div>
             <form onSubmit={handleSubmit} style={{ gap: 20 }}>
               <div className="mb-3" style={{ textAlign: "left" }}>
                 <label htmlFor="name" className="form-label">Name:</label>
@@ -124,7 +201,17 @@ function App() {
               <div className="mb-3" style={{ textAlign: "left", gap: 30 }}>
                 <label htmlFor="message" className="form-label">Message:</label>
                 <textarea className="form-control" id="message" name="message" value={formData.message} onChange={handleInputChange}></textarea>
-                <button type="submit" className="btn btn-success" style={{ textAlign: 'left' }}>Submit</button>
+              </div>
+              <div className="mb-3 justify-content-between d-flex" style={{ textAlign: "left", gap: 30 }}>
+                <button type="submit" className="btn btn-success" style={{ marginTop: 10 }}>
+                  <i className="fas fa-check m-1"></i>
+
+                  Submit</button>
+                <button type="button" className="btn btn-success" style={{ marginTop: 10 }}>
+                  <i className="fas fa-arrow-right m-1"></i>
+
+                  <NavLink to="/inventories/inventories" style={{ textDecoration: 'none', color: 'white' }}>Inventories</NavLink>
+                </button>
               </div>
             </form>
 
@@ -149,9 +236,12 @@ function App() {
                         <td>{item.email}</td>
                         <td>{item.message}</td>
 
-                        <td>
+                        <td className="justify-content-between align-items-center d-flex">
+                          {/* <button className="btn btn-primary" onClick={() => handleDelete(item._id)}>Edit</button> */}
+                          <button className="btn btn-primary" onClick={() => handleEdit(item)}>Edit</button>
                           <button className="btn btn-danger" onClick={() => handleDelete(item._id)}>Delete</button>
                         </td>
+
                       </tr>
                     ))}
                   </tbody>
@@ -161,6 +251,34 @@ function App() {
           </div>
         </div>
       </div>
+      
+      {/* Edit Modal */}
+      <Modal show={showModal} onHide={handleModalClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Form Data</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleModalSubmit}>
+            <div className="mb-3" style={{ textAlign: "left" }}>
+              <label htmlFor="name" className="form-label">Name:</label>
+              <input type="text" className="form-control" id="name" name="name" value={formData.name} onChange={handleInputChange} />
+            </div>
+            <div className="mb-3" style={{ textAlign: "left" }}>
+              <label htmlFor="email" className="form-label">Email:</label>
+              <input type="email" className="form-control" id="email" name="email" value={formData.email} onChange={handleInputChange} />
+            </div>
+            <div className="mb-3" style={{ textAlign: "left", gap: 30 }}>
+              <label htmlFor="message" className="form-label">Message:</label>
+              <textarea className="form-control" id="message" name="message" value={formData.message} onChange={handleInputChange}></textarea>
+            </div>
+            <div className="mb-3 justify-content-between d-flex" style={{ textAlign: "left", gap: 30 }}>
+              <Button variant="primary" type="submit">
+                Update
+              </Button>
+            </div>
+          </form>
+        </Modal.Body>
+      </Modal>
     </div>
   );
 }
